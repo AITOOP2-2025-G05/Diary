538b5bdb839b32fed3899813356ee4871e6abe2b
kuwabatakenonaka <k24060kk@aitech.ac.jp>
2025-10-16 15:44:27 +0900

    日記の追加/mainの一部変更

---
+++ diaries/KuwabaraDiary.py
@@ -0,0 +1,13 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class KuwabaraDiary(AbstractDiary):
+
+    def get_date(self):
+        return "2025-10-16"
+    
+    def get_summary(self):
+        return "とても眠たい1日"
+    
+    def get_author(self):
+        return "Kuwabara"
+    
